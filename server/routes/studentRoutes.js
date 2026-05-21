d215aa9a2b39ae6dfd9ff91b52161c1e8d37adc4
Viraj Madhavrao Jadhav <virajjadhav106@gmail.com>
2026-05-21 22:59:30 +0530

    Update studentRoutes.js

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -1,1 +1,12 @@
+const express = require("express");
 
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Student routes working"
+    });
+});
+
+module.exports = router;
