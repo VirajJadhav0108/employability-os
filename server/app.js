2d4ad915bcb2d6d741dea68887908ffc4e9b5e86
Viraj Madhavrao Jadhav <virajjadhav106@gmail.com>
2026-05-21 23:34:28 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/students", studentRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
