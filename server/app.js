8a2b1c7585d4cced4e0746cd35e5842b4713e9b3
Viraj Madhavrao Jadhav <virajjadhav106@gmail.com>
2026-05-21 23:32:53 +0530

    Add dotenv and database connection setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
 
+dotenv.config();
+
+connectDB();
 const studentRoutes = require("./routes/studentRoutes");
 
 const app = express();
