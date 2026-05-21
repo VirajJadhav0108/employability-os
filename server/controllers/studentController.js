3d14d5d7dfc646e37bd145d29b3b762c01fa423b
Viraj Madhavrao Jadhav <virajjadhav106@gmail.com>
2026-05-21 23:25:38 +0530

    Update studentController.js

--- server/controllers/studentController.js
+++ server/controllers/studentController.js
@@ -1,1 +1,10 @@
+const getStudents = (req, res) => {
+    res.json({
+        success: true,
+        message: "Fetched students successfully"
+    });
+};
 
+module.exports = {
+    getStudents
+};
