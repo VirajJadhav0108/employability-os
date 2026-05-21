028987c82cf7ec489f4c9a66060a914c15e2a521
Viraj Madhavrao Jadhav <virajjadhav106@gmail.com>
2026-05-21 23:29:14 +0530

    Update studentRoutes.js

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -3,9 +3,12 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    getStudents
+    getStudents,
+    createStudent
 } = require("../controllers/studentController");
 
 router.get("/", getStudents);
+
+router.post("/", createStudent);
 
 module.exports = router;
